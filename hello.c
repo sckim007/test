0bd628900c2513bdac392de7feb8e336e84f93cf
sckim007 <sckim007@gmail.com>
2018-07-27 10:39:45 +0900

    add login stage to JenkinsFile

--- hello.c
+++ hello.c
@@ -7,6 +7,7 @@ int main()
 	printf("hello jenkins !!\n");
 	printf("hello jenkins !!!\n");
 	printf("hello jenkins !!!!\n");
+	printf("hello jenkins (add login stage)!!!!\n");
 	printf("hello jenkins (add push stage)!!!!\n");
 	return 0;
 }
